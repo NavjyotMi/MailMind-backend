709a4db21e88f2a7131dff1d04a57b76fc581295
navjyot <navjyot.mishra.1@gmail.com>
2025-04-13 16:30:58 +0530

    no message

--- utils/Authorization.js
+++ utils/Authorization.js
@@ -2,6 +2,7 @@ const { verifytoken } = require("./jwtutility");
 
 module.exports.authenticateUser = function (req, res, next) {
   const access_token = req.headers.cookie;
+  console.log(access_token);
   if (!access_token) throw new Error("Access denied");
   const at = access_token.split("=")[1];
   const decoded = verifytoken(at);
